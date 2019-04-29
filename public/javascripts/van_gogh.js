0d535961c48a93f67a3c76dc52d7ffe530b0830c
Karli <karrlii@gmail.com>
2019-04-28 17:01:02 -0700

    testing mobile canvas load

--- public/javascripts/van_gogh.js
+++ public/javascripts/van_gogh.js
@@ -5,12 +5,12 @@ var myRec = new p5.SpeechRec(); // new P5.SpeechRec object
 myRec.continuous = true; // do continuous recognition
 myRec.interimResults = true; // allow partial recognition (faster, less accurate)
 
-let sessions = {};
+//https://github.com/processing/p5.js/wiki/Local-server
 
 var STROKE_WIDTH_FACTOR = 1.2;
 var STROKE_DISTANCE_FACTOR = 1.0;
 var CHEATING = true;
-
+var b = new Brush();
 var BLUE = 1;
 var YELLOW = 2;
 var BROWN = 3;
@@ -23,6 +23,7 @@ var skyHuesHSB = [
     [213, 99, 52],
     [207, 95, 80]
 ];
+// var skyHuesHSB = [[180, 19, 100], [180, 28, 100], [196, 47, 100], [191, 45, 100], [203, 39, 100], [191, 50, 83]];
 
 var brownHues = ['#2A0F14', '#301816', '#2C1821', '#1A1422', '#22101C', '#19121A'];
 var greenHues = ['#111828', '#021621', '#071226', '#040420', '#20272D', '#112120', '#14222D'];
@@ -36,12 +37,14 @@ var whiteHuesHSB = [
     [55, 41, 98]
 ];
 
+let img;
 
-//object Oriented Collision
-var scrollingCanvas;
+var renderOverlay = true;
+var outlineOverlay = true;
 
-var canvasWidth = 1493;
-var canvasHeight = 1200;
+var buttonA, buttonB, buttonC, buttonD;
+var colorSN_img1, colorSN_img2, colorSN_img3, clearImage;
+var blur_img; // image used to reference for brightness
 
 let colorBackground;
 let mainCanvas;
@@ -80,9 +83,11 @@ let yOffest = 0.0;
 let alpha = 255;
 
 function preload() {
+    road_set = loadImage('images/road.png');
+
     // create a new Layer to load these Images onto
-    colorSN = createGraphics(canvasWidth, canvasHeight);
-    clearImage = createGraphics(canvasWidth, canvasHeight);
+    colorSN = createGraphics(1493, 1200);
+    clearImage = createGraphics(1493, 1200);
     clearImage.fill(255);
     clearImage.noStroke();
     clearImage.rect(0, 0, clearImage.width, clearImage.height);
@@ -91,17 +96,52 @@ function preload() {
     colorSN_img3 = colorSN.loadImage('images/monochorme faded.png');
     colorSN_img4 = colorSN.loadImage('images/outline.png');
     blur_img = loadImage('images/grayscale blur.png');
-    road_set = loadImage('images/road.png');
+}
+
+function getImageBrightness(x, y, actual, reference) {
+    let percentX = x / actual.width;
+    let percentY = y / actual.height;
+    let referenceX = floor(percentX * reference.width);
+    let referenceY = floor(percentY * reference.height);
+    let color = reference.get(referenceX, referenceY);
+
+    let actualX = floor(percentX * actual.width);
+    let actualY = floor(percentY * actual.height);
+
+    if (CHEATING) {
+        return actual.get(actualX, actualY);
+    }
+    // add exception for brown brush
+    if (brushType === BROWN) {
+        return color[0] * 0.2;
+    }
+    return color[0];
 }
 
 
 function setup() {
-    createCanvas(canvasWidth, canvasHeight);
+    createCanvas(canvas_width, canvas_height);
     backgroundImage = ['images/monochorme faded.png', 'images/monochorme.png', 'images/outline.png']
 
+
     // create the drawing layer for the brush
-    mainCanvas = createGraphics(canvasWidth, canvasHeight);
+    mainCanvas = createGraphics(canvas_width, canvas_width);
     mainCanvas_img = mainCanvas.loadImage(backgroundImage[2]);
+    background(255, 255, 255, 0);
+    colorMode(HSB);
+
+    // generate a 2D array to hold the state of each grid cell
+    grid = create2DArray(grid_cols, grid_rows, false);
+    console.log(grid);
+    // populate an initial drawing
+    grid[6][4] = true;
+    grid[7][4] = true;
+    grid[8][4] = true;
+    grid[7][3] = true;
+    grid[7][5] = true;
+
+    noSmooth();
+
 
     grid_settings = {
         grid_cols: grid_cols,
@@ -115,7 +155,6 @@ function setup() {
     r = new rectObj(random(width), random(height), boxSize, boxSize, my_color, [], grid_settings, sessionId);
 
 
-    grid = create2DArray(grid_cols, grid_rows, true);
 
     io.on('mouse', function(data, sessionId) {
         let updated = false;
@@ -140,9 +179,36 @@ function setup() {
 }
 
 
+function toggleTile(mouseX, mouseY) {
+    // find the grid location of the click
+    let grid_x = floor(mouseX / col_width);
+    let grid_y = floor(mouseY / col_width);
+
+    // toggle the cell state
+    grid[grid_x][grid_y] = !grid[grid_x][grid_y];
+
+    redraw();
+}
+
+
 function draw() {
     background(255);
-    //drawMap();
+
+    if (mouseIsPressed) {
+        b.update(mouseX, mouseY);
+        b.render();
+    } else {
+        b.endLine();
+    }
+
+    //previewColoredImage();
+    image(mainCanvas, 0, 0, width, height);
+
+    if (outlineOverlay == true) {
+        image(colorSN_img4, 0, 0, width, height);
+    }
+
+
     drawGrid();
 
     r.update();
@@ -189,6 +255,222 @@ function touchEnded() {
 function emit(eventName, data) {
     io.emit(eventName, data, sessionId);
 }
+
+function previewColoredImage() {
+    image(colorSN_img1, 0, 0, width, height);
+}
+
+function clearPreview() {
+    image(clearImage, 0, 0, width, height);
+}
+
+function BackgroundSelector4() {
+    //image(mainCanvas_img, 0, 0, width, height);
+    //clear();
+    mainCanvas.clear();
+}
+
+function BackgroundSelector(bgSelect) {
+    if (bgSelect == 'white') {
+        image(clearImage, 0, 0, width, height);
+    } else if (bgSelect == 'outline') {
+        outlineOverlay = !outlineOverlay;
+    } else if (bgSelect == 'monochrome faded') {
+        image(colorSN_img3, 0, 0, width, height);
+    }
+}
+
+// HTML Navigation
+
+function toggleMenu() {
+    const menu = document.querySelector('.menu');
+    menu.addEventListener('click', menu.classList.toggle('menuActive'));
+}
+
+function brushSelector(brushInput) {
+    if (brushInput == 'blue') {
+        brushType = BLUE;
+    } else if (brushInput == 'yellow') {
+        brushType = YELLOW;
+    } else if (brushInput == 'brown') {
+        brushType = BROWN;
+    }
+}
+
+function BrushSegment(x, y, stroke, weight, brightness) {
+    this.x = x;
+    this.y = y;
+    this.stroke = stroke;
+    this.weight = weight;
+    this.brightness = brightness;
+}
+
+function Brush() {
+    this.drawing = false;
+    this.upperHistory = [];
+    this.lowerHistory = [];
+
+    this.update = function(x, y) {
+        if (!this.drawing) {
+            // initial settings
+            this.new = createVector(x, y);
+            this.last = createVector(x, y);
+            this.upper = createVector(x, y);
+            this.lower = createVector(x, y);
+
+            // set colors based on brush type
+            if (brushType == BLUE) {
+                this.upperColor = random(skyHuesHSB);
+                this.lowerColor = random(skyHuesHSB);
+            } else if (brushType == YELLOW) {
+                this.upperColor = random(whiteHuesHSB);
+                this.lowerColor = random(whiteHuesHSB);
+            } else {
+                this.upperColor = random(brownHuesHSB);
+                this.lowerColor = random(brownHuesHSB);
+            }
+
+            this.lastSize = false;
+            this.upperWidth = random(0.7, 1.5);
+            this.lowerWidth = random(0.8, 1.75);
+            this.lineLength = random(350, 550) * STROKE_DISTANCE_FACTOR;
+            this.drawing = true;
+        } else {
+            this.new.x = lerp(this.last.x, x, viscosity);
+            this.new.y = lerp(this.last.y, y, viscosity);
+        }
+
+        let distance = this.last.dist(this.new);
+        this.distanceMoved += distance;
+        this.heading = this.new.copy().sub(this.last).heading();
+
+        let newSize = this.mapWeight(distance);
+        let dist = this.mapDistance(distance)
+        let limit = this.lineLength * 0.25;
+
+        // add dampening at start and end of lines
+        if (this.lineLength - this.distanceMoved < limit) {
+            let remaining = this.lineLength - this.distanceMoved;
+            if (remaining <= 1) {
+                remaining = 1;
+            }
+            let percentageRemaining = remaining / limit;
+            dist = this.mapDistance(distance * percentageRemaining);
+            newSize = this.mapWeight(distance * percentageRemaining);
+        } else if (this.distanceMoved < limit) {
+            let percentageRemaining = this.distanceMoved / limit;
+            dist = this.mapDistance(distance * percentageRemaining);
+            newSize = this.mapWeight(distance * percentageRemaining);
+        }
+
+        // initial
+        if (this.lastSize === false) {
+            this.lastSize = newSize;
+        }
+        let size = lerp(this.lastSize, newSize, viscosity / 2);
+        let uy = -cos(this.heading) * dist;
+        let ux = sin(this.heading) * dist;
+        let ly = cos(this.heading) * dist;
+        let lx = -sin(this.heading) * dist;
+
+        let newUpperX = this.new.x + ux;
+        let newUpperY = this.new.y + uy;
+        let newLowerX = this.new.x + lx;
+        let newLowerY = this.new.y + ly;
+
+        if (this.distanceMoved > 10) {
+            // upper stroke
+            this.upperHistory.push(new BrushSegment(
+                this.upper.x,
+                this.upper.y,
+                this.upperColor,
+                size * this.upperWidth,
+                getImageBrightness(
+                    (this.upper.x + newUpperX) / 2,
+                    (this.upper.y + newUpperY) / 2,
+                    colorSN_img1,
+                    blur_img
+                )
+            ));
+
+            // lower stroke
+            this.lowerHistory.push(new BrushSegment(
+                this.lower.x,
+                this.lower.y,
+                this.lowerColor,
+                size * this.lowerWidth,
+                getImageBrightness(
+                    (this.lower.x + newLowerX) / 2,
+                    (this.lower.y + newLowerY) / 2,
+                    colorSN_img1,
+                    blur_img
+                )
+            ));
+        }
+
+        // end stroke based on length
+        if (this.distanceMoved > this.lineLength) {
+            this.endLine()
+        }
+
+        // end stroke on sharp turns
+        if (this.distanceMoved > 100 && abs((degrees(this.heading) + 720) - (degrees(this.lastHeading) + 720)) > 60) {
+            this.endLine()
+        }
+
+        // set state for next frame
+        this.lastSize = size;
+        this.lastHeading = this.heading;
+        this.upper.set(newUpperX, newUpperY);
+        this.lower.set(newLowerX, newLowerY);
+        this.last.set(this.new);
+    };
+
+    this.renderLine = function(history) {
+        colorMode(HSB);
+        let lerpBrightness;
+
+        for (let i = 0; i < history.length - 1; i++) {
+            let cur = history[i];
+            let next = history[i + 1];
+            if (i === 0) {
+                lerpBrightness = cur.brightness;
+            }
+            if (CHEATING) {
+                colorMode(RGB, 255);
+                brightness = lerpColor(color(lerpBrightness), color(next.brightness), viscosity);
+                mainCanvas.stroke(brightness);
+            } else {
+                brightness = lerp(lerpBrightness, next.brightness, viscosity);
+                mainCanvas.stroke(color(cur.stroke[0], cur.stroke[1], brightness));
+            }
+            mainCanvas.strokeWeight(cur.weight);
+            mainCanvas.line(cur.x, cur.y, next.x, next.y);
+            lerpBrightness = brightness;
+        }
+    }
+
+    this.endLine = function() {
+        this.drawing = false;
+        this.distanceMoved = 0;
+        this.upperHistory = [];
+        this.lowerHistory = [];
+    }
+
+    this.mapWeight = function(distance) {
+        return map(distance, 0, 30, 6 * STROKE_WIDTH_FACTOR, 12 * STROKE_WIDTH_FACTOR);
+    }
+
+    this.mapDistance = function(distance) {
+        return map(distance, 0, 36, 2 * STROKE_DISTANCE_FACTOR, 15 * STROKE_DISTANCE_FACTOR);
+    }
+
+    this.render = function() {
+        this.renderLine(this.upperHistory);
+        this.renderLine(this.lowerHistory);
+    }
+}
+
 
 function rectObj(x, y, w, h, color, others, grid_settings, sessionId) {
     this.x = x
@@ -311,44 +593,6 @@ function rectObj(x, y, w, h, color, others, grid_settings, sessionId) {
     }
 }
 
-function previewColoredImage() {
-    image(colorSN_img1, 0, 0, width, height);
-}
-
-function clearPreview() {
-    image(clearImage, 0, 0, width, height);
-}
-
-function BackgroundSelector4() {
-    mainCanvas.clear();
-}
-
-function BackgroundSelector(bgSelect) {
-    if (bgSelect == 'white') {
-        image(clearImage, 0, 0, width, height);
-    } else if (bgSelect == 'outline') {
-        outlineOverlay = !outlineOverlay;
-    } else if (bgSelect == 'monochrome faded') {
-        image(colorSN_img3, 0, 0, width, height);
-    }
-}
-
-// HTML Navigation
-
-function toggleMenu() {
-    const menu = document.querySelector('.menu');
-    menu.addEventListener('click', menu.classList.toggle('menuActive'));
-}
-
-function brushSelector(brushInput) {
-    if (brushInput == 'blue') {
-        brushType = BLUE;
-    } else if (brushInput == 'yellow') {
-        brushType = YELLOW;
-    } else if (brushInput == 'brown') {
-        brushType = BROWN;
-    }
-}
 
 function drawMap() {
     // loop over each cell
