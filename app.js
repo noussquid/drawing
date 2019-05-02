a796b088703abcdadee728e0ce932aa74dafa089
noussquid <karrli@gmail.com>
2019-05-02 22:16:16 +0000

    added https support

--- app.js
+++ app.js
@@ -4,15 +4,22 @@ var express = require('express')
   , routes = require('./routes')
   , user = require('./routes/user')
   , http = require('http')
-  , path = require('path');
-
+  , path = require('path')
+  , cookieParser = require('cookie-parser')
+  , http = require('http')
+  , https = require('https')
+  , session = require('express-session');
+     
 var app = express();
+// Sessions
+    app.use(cookieParser());
+    app.use(session({secret: 'secret', key: 'express.sid'}));
 
 app.engine('html', require('ejs').renderFile);
 
 // Configure our application
 app.configure(function(){
-  app.set('port', process.env.PORT || 3000);
+  app.set('port', process.env.PORT || 8080);
   app.set('views', __dirname + '/views');
 //app.set('view engine', 'jade');
   app.set('view engine', 'html'); 
